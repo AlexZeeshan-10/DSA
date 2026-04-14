0aeb5fba38bb17dc3028a090f6bc0dfbce0f509e
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-15 01:11:34 +0530

    left rotate and move zeros

--- 18-array_union_and_intsec.cpp
+++ 18-array_union_and_intsec.cpp
@@ -1,25 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void left_rotate(vector<int> &arr){
-    if (arr.empty()) return;
-    int temp = arr[0];
-    for (int i = 1; i < arr.size(); i++)
-    {
-        arr[i-1] = arr[i];
+void left_rotate(vector<int> &arr, int count){
+    // count %= arr.size();
+    // while(count--){
+    //     if (arr.empty()) return;
+    // int temp = arr[0];
+    // for (int i = 1; i < arr.size(); i++)
+    // {
+    //     arr[i-1] = arr[i];
+    // }
+    // arr[arr.size() - 1] = temp;
+    // }
+    reverse(arr.begin(), arr.begin()+count);
+    reverse(arr.begin()+count, arr.end());
+    reverse(arr.begin(), arr.end());
+}
+//Move zeros to end
+void move_zero(vector<int> &arr){
+    for (int i = 1; i < arr.size(); i++){
+        for (int j = 0; j < arr.size()-1; j++){
+               if(arr[i] != 0 && arr[j] == 0){
+                swap(arr[i], arr[j]);
+               }
+        }
     }
-    arr[arr.size() - 1] = temp;
 }
 
 int main(){  
-    int n;
+    int n, count;
     cin >> n;
     vector<int> arr(n);
     for (int i = 0; i < arr.size(); i++){
         cin >> arr[i];
     }
 
-    left_rotate(arr);
+    cin >> count; 
+    move_zero(arr);
 
     for (int i = 0; i < arr.size(); i++){
         cout << arr[i] << " ";
