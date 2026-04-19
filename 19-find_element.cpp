a3d9d4c8cd90e27577d9ebf214020cbcbcf8d5e4
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-19 16:21:43 +0530

    find number

--- 19-find_element.cpp
+++ 19-find_element.cpp
@@ -1,14 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> missing(vector<int> &arr1)
+int missing_no(vector<int> &arr1)
 {
 
+    // Initialize the vector with zeros and enough size to prevent out-of-bounds access.
+    vector<int> hash(arr1.size() + 2, 0);
+    for (int i = 0; i < arr1.size(); i++)
+    {
+        hash[arr1[i]] = 1;
+    }
+    for (int i = 1; i < hash.size(); i++)
+    {
+        if(hash[i] == 0) return i;
+    }
+    return -1;
+}
+
+int OPmissing_no(vector<int> arr1){
+    int n = arr1.size();
+    int s1, s2 = 0;
+    s1 = n * (n+1) / 2;
+    for (int i = 0; i < n; i++)
+    {
+        s2 += arr1[i];
+    }
+    return s1-s2;
 }
 
 int main()
 {
-    int s1, s2;
+    int s1;
+    int s2;
     cin >> s1;
     vector<int> arr1(s1);
     for (int i = 0; i < arr1.size(); i++)
@@ -16,16 +39,13 @@ int main()
         cin >> arr1[i];
     }
 
-    cin >> s2;
-    vector<int> arr2(s2);
-    for (int i = 0; i < arr2.size(); i++)
-    {
-        cin >> arr2[i];
-    }
+    // cin >> s2;
+    // vector<int> arr2(s2);
+    // for (int i = 0; i < arr2.size(); i++)
+    // {
+    //     cin >> arr2[i];
+    // }
 
-    vector<int> s = missing(arr1);
-    for (int i = 0; i < s.size(); i++)
-    {
-        cout << s[i] << " ";
-    }
+    int s = OPmissing_no(arr1);
+    cout << s << endl;
 }
