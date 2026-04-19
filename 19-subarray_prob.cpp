6ff214f8bd14b696065e8d555ddd23b38720e8a6
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-20 00:48:35 +0530

    completed vid 19

---
+++ 19-subarray_prob.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longest_subarray(vector<int> &arr, int k)
+{
+    long long sum = arr[0];
+    int maxLen = 0;
+    int n = arr.size();
+    int left = 0, right = 0;
+    while(right < n){
+        while(left <= right && sum>k){
+            sum -= arr[left];
+            left++;
+        }
+        if(sum == k){
+            maxLen = max(maxLen, right - left+1);
+        }
+        right++;
+        if(right < n) sum += arr[right];
+    }
+    return maxLen;
+}
+
+int main()
+{
+    int n, k;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> k;
+    int s = longest_subarray(arr, k);
+    cout << s << " ";
+}
