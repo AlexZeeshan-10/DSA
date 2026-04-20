d4cd05151bd139d80cbbc4dc4cb1c99a963ed62e
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-21 01:18:47 +0530

    sort

---
+++ 22-sort.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//sort 0s, 1s and 2s normal approach
+vector<int> sort(vector<int> &arr){
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i] == 0) cnt0++;
+        if(arr[i] == 1) cnt1++;
+        if(arr[i] == 2) cnt2++;
+    }
+    for (int i = 0; i < cnt0; i++)
+    {
+        arr[i] = 0;
+    }
+    for (int i = cnt0; i < cnt0+cnt1; i++)
+    {
+        arr[i] = 1;
+    }
+    for (int i = cnt0+cnt1; i < arr.size(); i++)
+    {
+        arr[i] = 2;
+    }
+    
+    return arr;
+}
+
+//sort 0s, 1s and 2s using Deutch National Flag Algorithm this would be an optimal approach
+vector<int> OPsort(vector<int> &arr){
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+    
+    vector<int> ans;
+    ans = sort(arr);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    
+     
+}
