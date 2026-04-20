3789a3823f332ab4287ded375aedb16d74ffa2bc
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-20 23:59:53 +0530

    two sum optimised solution

--- 21-two_sum.cpp
+++ 21-two_sum.cpp
@@ -1,24 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-string two_sum(vector<int> &arr, int s){
-    int left = 0, right = arr.size()-1;;
+string two_sum(vector<int> &arr, int s)
+{
+    int left = 0, right = arr.size() - 1;
+    ;
     sort(arr.begin(), arr.end());
-    while (left<right)
+    while (left < right)
     {
         int sum = arr[left] + arr[right];
-        if(sum == s) return "YES";
-        else if(sum<s) left++;
-        else right--;
+        if (sum == s)
+            return "YES";
+        else if (sum < s)
+            left++;
+        else
+            right--;
     }
     return "NO";
 }
 
-int OPtwo_sum(vector<int> &arr, int s){
-    
+vector<int> OPtwoSum(vector<int> &nums, int target)
+{
+    map<int, int> mpp;
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        int num = nums[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end())
+        {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return {-1, -1};
 }
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n);
@@ -28,6 +47,11 @@ int main(){
     }
     int s = 0;
     cin >> s;
-    string ans = two_sum(arr, s);
-    cout << ans << " ";
+    vector<int> ans;
+    ans = OPtwoSum(arr, s);
+    for (int num : ans)
+    {
+        cout << num << " ";
+    }
+    
 }
