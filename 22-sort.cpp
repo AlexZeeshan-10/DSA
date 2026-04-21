75497ec34c78790354cd4d4ee534ee8facc1827b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-21 21:45:51 +0530

    Optimal code for sorting 0s, 1s and 2s

--- 22-sort.cpp
+++ 22-sort.cpp
@@ -1,32 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-//sort 0s, 1s and 2s normal approach
-vector<int> sort(vector<int> &arr){
+// sort 0s, 1s and 2s normal approach
+vector<int> sort(vector<int> &arr)
+{
     int cnt0 = 0, cnt1 = 0, cnt2 = 0;
     for (int i = 0; i < arr.size(); i++)
     {
-        if(arr[i] == 0) cnt0++;
-        if(arr[i] == 1) cnt1++;
-        if(arr[i] == 2) cnt2++;
+        if (arr[i] == 0)
+            cnt0++;
+        if (arr[i] == 1)
+            cnt1++;
+        if (arr[i] == 2)
+            cnt2++;
     }
     for (int i = 0; i < cnt0; i++)
     {
         arr[i] = 0;
     }
-    for (int i = cnt0; i < cnt0+cnt1; i++)
+    for (int i = cnt0; i < cnt0 + cnt1; i++)
     {
         arr[i] = 1;
     }
-    for (int i = cnt0+cnt1; i < cnt0+cnt1+cnt2; i++)
+    for (int i = cnt0 + cnt1; i < cnt0 + cnt1 + cnt2; i++)
     {
         arr[i] = 2;
     }
-    
+
     return arr;
 }
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n);
@@ -34,13 +38,11 @@ int main(){
     {
         cin >> arr[i];
     }
-    
+
     vector<int> ans;
     ans = sort(arr);
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
     }
-    
-     
 }
