aa4321123e868a8c2f6fcd5689f5fb98b08a4e97
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-22 23:47:44 +0530

    moore voting algo done

--- 23-Majority_ele.cpp
+++ 23-Majority_ele.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-//counting majority element in an array
-int major_ele(vector<int> &arr){
+// counting majority element in an array
+int major_ele(vector<int> &arr)
+{
     map<int, int> mpp;
 
     // occupies more lines of code
@@ -21,13 +21,16 @@ int major_ele(vector<int> &arr){
             return i->first;
         }
     } */
-    
-    //occupies less lines and easy to understand
-    for(auto x : arr) mpp[x]++;
+
+    // occupies less lines and easy to understand
+    for (auto x : arr)
+        mpp[x]++;
 
     int maxi = 0, ans = 0;
-    for(auto it : mpp){
-        if(maxi < it.second){
+    for (auto it : mpp)
+    {
+        if (maxi < it.second)
+        {
             maxi = it.second;
             ans = it.first;
         }
@@ -35,13 +38,46 @@ int major_ele(vector<int> &arr){
     return ans;
 }
 
-int main(){
+// Moore's Voting algorithm for Majority element
+int moore_algo(vector<int> &arr)
+{
+    int ele = 0, count = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (count == 0)
+        {
+            ele = arr[i];
+            count++;
+        }
+        else if (count > 0 && arr[i] != ele)
+        {
+            count--;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    int count1 = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == ele)
+            count1++;
+    }
+    if (count1 > (arr.size() / 2))
+        return ele;
+    return -1;
+}
+
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n);
-    for(int i=0; i < arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++)
+    {
         cin >> arr[i];
     }
-    int x = major_ele(arr);
+    int x = moore_algo(arr);
     cout << x;
 }
