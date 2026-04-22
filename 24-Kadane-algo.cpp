8f3acc85126f41941dc6fb3a06020a0a7ae9f702
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-23 00:22:35 +0530

    brute force method of max sum

--- 24-Kadane-algo.cpp
+++ 24-Kadane-algo.cpp
@@ -1,17 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// brute force approach to find max sum of subarray
+int maxSum_subarray(vector<int> &arr)
+{
+    int sum = 0, maxi = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum = 0;
+        for (int j = i; j < arr.size(); j++)
+        {
+            sum += arr[j];
+            maxi = max(maxi, sum);
+        }
+    }
+    return maxi;
+}
 
-
-
-int main(){
+int main()
+{
 
     int n;
     cin >> n;
     vector<int> arr(n);
-    for (int i = 0; i < arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++)
+    {
         cin >> arr[i];
     }
-    
-
 }
