6dca68e43ed19234e9d5b9746c3c6128bd76a814
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-24 01:36:15 +0530

    kadane algo complete

--- 24-Kadane-algo.cpp
+++ 24-Kadane-algo.cpp
@@ -1,20 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Optimal approach for Max sum of subarray
+int Kadane_algo(vector<int> &arr)
+{
+    int sum = 0, maxi = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+        if (sum > maxi)
+        {
+            maxi = sum;
+        }
 
-//Optimal approach for Max sum of subarray
-int Kadane_algo(vector<int> &arr){
-    
+        if (sum < 0)
+            sum = 0;
+    }
+    if (maxi < 0)
+        return 0;
+    else
+        return maxi;
 }
 
-int main(){
+int main()
+{
 
     int n;
     cin >> n;
     vector<int> arr(n);
-    for (int i = 0; i < arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++)
+    {
         cin >> arr[i];
     }
+    int ans = Kadane_algo(arr);
     
-
+    cout << ans;
 }
