b93764e725c413c24881df6f82c15a86a97ebbf5
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-24 01:41:47 +0530

    both methods in main

--- 24-Kadane-algo.cpp
+++ 24-Kadane-algo.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// brute force approach to find max sum of subarray
+int maxSum_subarray(vector<int> &arr)
+{
+    int sum = 0, maxi = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum = 0;
+        for (int j = i; j < arr.size(); j++)
+        {
+            sum += arr[j];
+            maxi = max(maxi, sum);
+        }
+    }
+    return maxi;
+}
+
+
 // Optimal approach for Max sum of subarray
 int Kadane_algo(vector<int> &arr)
 {
@@ -21,6 +39,7 @@ int Kadane_algo(vector<int> &arr)
     else
         return maxi;
 }
+
 
 int main()
 {
