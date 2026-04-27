f4ce3a6c99efb9f61ab1d685a768c3ee8eb97162
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-28 00:52:38 +0530

    added brute and optimal code for leader in array

--- 28-leaderArray.cpp
+++ 28-leaderArray.cpp
@@ -1,6 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute-force approach
+vector<int> lead(vector<int> &arr){
+    vector<int> temp;
+    for (int i = 0; i < arr.size(); i++)
+    {   bool lead = true;
+        for (int j = i+1; j < arr.size(); j++)
+        {
+            if(arr[i] < arr[j]) {
+            lead = false;
+            break;
+            }
+        }
+        if(lead == true){
+            temp.push_back(arr[i]);
+        }
+    }
+    return temp;
+}
+
+// Optimal O(N) Approach
+vector<int> optimalLead(vector<int> &arr){
+    vector<int> ans;
+    if(arr.empty()) return ans;
+    
+    int maxi = arr[arr.size() - 1];
+    ans.push_back(maxi); // The rightmost element is always a leader
+    
+    for (int i = arr.size() - 2; i >= 0; i--) {
+        if (arr[i] >= maxi) {
+            ans.push_back(arr[i]);
+            maxi = arr[i];
+        }
+    }
+    
+    reverse(ans.begin(), ans.end()); // Reverse to maintain the original relative order
+    return ans;
+}
+
 int main()
 {
     int n;
@@ -11,5 +49,10 @@ int main()
         cin >> arr[i];
     }
     vector <int> ans;
-    ans = (arr);
+    ans = optimalLead(arr);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    
 }
