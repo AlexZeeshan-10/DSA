b1c04f9517c0a2e360b7dc8bbb442f713ee0e95d
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-04-30 00:12:29 +0530

    added comment for brute force method and built a 2D array to store elemets

--- 30-setMatrix_zeros.cpp
+++ 30-setMatrix_zeros.cpp
@@ -1,13 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+The brute force solution takes almost n^3 time complexity so I'm skipping that for now for reference visit the video 30 once again. That was not that complicated you just need to run an iteration for looking out for zeros in the matrix and marking them as -1 to mark them you needed another funtion which occupied another time complexity. Finally you would run another iteration to mark all the -1 as 0 to completely replace all the ones as zeros in the adjacent column and row.
+*/
+
+
+// Brute for approach to set zeros in the matrix
+
+
 int main(){
-    int n;
+    int n, m;
+    cin >> m;
     cin >> n;
-    vector<int> arr(n, 0);
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cin >> arr[i];
+    
+    // Create a 2D vector (matrix) with 'm' rows and 'n' columns, initialized to 0
+    vector<vector<int>> matrix(m, vector<int>(n, 0));
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
     }
        
 }
