45b68e65660ceb39ae28888cf9153ce17c1fc357
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-01 01:07:04 +0530

    added rotate function

--- 31-rotateMatrix_90.cpp
+++ 31-rotateMatrix_90.cpp
@@ -1,6 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//Optimal approach to rotate matrix by clockwise 90 Degrees
+void rotateMat(vector<vector <int>> &mat){
+    int row = mat.size();
+    int col = mat[0].size();
+
+    // Step 1: Transpose the matrix (swap rows with columns)
+    for (int i = 0; i < row-1; i++)
+    {
+        for (int j = i+1; j < col; j++)
+        {
+            swap(mat[i][j], mat[j][i]);
+        }
+        
+    }
+    
+    // Step 2: Reverse each row individually to complete the clockwise rotation
+    for (int i = 0; i < row; i++)
+    {
+        reverse(mat[i].begin(), mat[i].end());
+    }
+    
+}
+
+
 int main(){
     int m, n;
     cin >> m;
@@ -15,13 +40,15 @@ int main(){
         
     }
     
-    vector<vector <int>> ans;
+    rotateMat(matrix);
+
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << ans[i][j];
+            cout << matrix[i][j] << " ";
         }
+        cout << endl;
     }
     
 }
