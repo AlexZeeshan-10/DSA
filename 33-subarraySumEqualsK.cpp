fd5f34f07fd4944bf7f0efe1d42b64f392109464
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-02 00:20:23 +0530

    working on feature

--- 33-subarraySumEqualsK.cpp
+++ 33-subarraySumEqualsK.cpp
@@ -3,20 +3,32 @@ using namespace std;
 
 
 // Better solution for Subarray sum that is equal to k
-int subArraySum(vector<int> &arr){
-    
+int subArraySum(vector<int> &arr, int k){
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int sum = 0; // Reset sum for each new starting index
+        for (int j = i; j < arr.size(); j++)
+        {
+            sum += arr[j];
+            if(sum == k) count++;
+        }
+        
+    }
+    return count;
 }
 
 int main(){
-    int n;
+    int n, k;
     cin >> n;
+    cin >> k;
     vector<int> arr(n, 0);
     for (int i = 0; i < arr.size(); i++)
     {
         cin >> arr[i];
     }
     
-    int ans = subArraySum(arr);
-
+    int ans = subArraySum(arr, k);
+    cout << ans << " ";
      
 }
