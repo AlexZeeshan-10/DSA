01700f0da19b59e122e0afbfdab8ff8a6874a1f3
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-04 16:12:22 +0530

    optimised feature added vid-35

--- 35-majorityElemnt2.cpp
+++ 35-majorityElemnt2.cpp
@@ -20,6 +20,40 @@ vector<int> majorElementbtr(vector<int> &arr){
     return ls;
 }
 
+//Majority element in an array optimal solution Moore's algo for two major elements in an array
+vector<int> majorElementOpt(vector<int> &arr){
+    int count1 = 0, count2 = 0;
+    int ele1 = INT_MIN, ele2 = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(count1 == 0 && ele2 != arr[i]){
+            count1 = 1;
+            ele1 = arr[i];
+        }
+        else if(count2 == 0 && ele2 != arr[i]){
+            count2 = 1;
+            ele2 = arr[i];
+        }
+        else if (arr[i] == ele1) count1++;
+        else if (arr[i] == ele2) count2++;
+        else{
+            count1--;
+            count2--;
+        }
+    }
+    vector<int> ls;
+    count1 = 0, count2 = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(ele1 == arr[i]) count1++;
+        if(ele2 == arr[i]) count2++;
+    }
+    int mini = (int)(arr.size()/3) + 1;
+    if(count1 >= mini) ls.push_back(ele1);
+    if(count2 >= mini) ls.push_back(ele2);
+    sort(ls.begin(), ls.end());
+    return ls;
+}
 
 int main(){
     int n;
@@ -30,7 +64,7 @@ int main(){
         cin >> arr[i];
     }
     
-    vector<int> ans = majorElementbtr(arr);
+    vector<int> ans = majorElementOpt(arr);
 
     for (int i = 0; i < ans.size(); i++)
     {
