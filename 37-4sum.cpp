a555cc7ea1946717ada17af40ff9d633c5e6ac4b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-05 22:03:15 +0530

    function added for better solution

--- 37-4sum.cpp
+++ 37-4sum.cpp
@@ -10,7 +10,19 @@ vector<vector<int>> fourSumbtr(vector<int> &arr, int target)
     {
         for (int j = i + 1; j < n; j++)
         {
-            
+            set<long long> hashset;
+            for (int k = j + 1; k < n; i++)
+            {
+                long long sum = arr[i] + arr[j];
+                sum += arr[k];
+                long long fourth = target - (sum);
+                if (hashset.find(fourth) != hashset.end())
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[fourth]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
         }
     }
     vector<vector<int>> ans(st.begin(), st.end());
