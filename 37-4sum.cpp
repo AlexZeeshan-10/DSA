d5f547b5f515bbfe7cb43ed65778e3614852fc99
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-05 22:27:40 +0530

    structure optimal code

--- 37-4sum.cpp
+++ 37-4sum.cpp
@@ -33,6 +33,26 @@ vector<vector<int>> fourSumbtr(vector<int> &arr, int target)
 vector<vector<int>> fourSumOpt(vector<int> &arr, int target)
 {
     
+    int n = arr.size();
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && arr[i] == arr[i - 1])
+            continue;
+        for (int j = i + 1; i < n; j++)
+        {
+            if (j != (i + 1) && arr[j] == arr[j - 1])
+                continue;
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l)
+            {
+                
+            }
+        }
+    }
+    return ans;
 }
 
 int main()
