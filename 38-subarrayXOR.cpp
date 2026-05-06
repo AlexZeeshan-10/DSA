31e435eb86b612dda3a573756bcbb30e69f2583e
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-06 13:02:58 +0530

    beginning vid-38

---
+++ 38-subarrayXOR.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+    
+}
