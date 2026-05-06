e0a3a898364fe8b87379b3105891155b9405a70c
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-07 00:53:11 +0530

    better function added

--- 38-subarrayXOR.cpp
+++ 38-subarrayXOR.cpp
@@ -1,11 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int subXORbtr(vector<int> &arr, int n){
-    
+int subXORbtr(vector<int> &arr, int k)
+{
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int XOR = 0;
+        for (int j = 0; j < arr.size(); j++)
+        {
+            XOR = XOR ^ arr[j];
+            if (XOR == k)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n, 0);
@@ -13,7 +28,7 @@ int main(){
     {
         cin >> arr[i];
     }
-    
+
     int ans = subXORbtr(arr, k);
     cout << ans << " ";
 }
