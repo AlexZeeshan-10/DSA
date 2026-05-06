c41913a10fbf51527fa02d1659285a710d49433d
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-07 01:08:55 +0530

    opt feature added

--- 38-subarrayXOR.cpp
+++ 38-subarrayXOR.cpp
@@ -21,6 +21,18 @@ int subXORbtr(vector<int> &arr, int k)
 
 int subXORopt(vector<int> &arr, int k)
 {
+    int xr = 0;
+    map<int, int> mpp;
+    mpp[0] = 1;
+    int cnt = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        xr = xr ^ arr[i];
+        int x = xr ^ k;
+        cnt += mpp[x];
+        mpp[xr]++;
+    }
+    return cnt;
 }
 
 int main()
