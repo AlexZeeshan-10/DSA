ac927627a87a061572f86ace3f5a46ca4147cd4a
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-08 00:28:49 +0530

    beginning vid-39

---
+++ 39-merge_Overlapping.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> arr;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            cin >> arr[i][j];
+        }
+        
+    }
+}
