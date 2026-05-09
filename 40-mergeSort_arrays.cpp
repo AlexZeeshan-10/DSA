be8d4f749e32f4d3f1b81b0f4a45eee727b5a60d
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-10 02:17:07 +0530

    added minor changes with comments

--- 40-mergeSort_arrays.cpp
+++ 40-mergeSort_arrays.cpp
@@ -1,21 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*To test the code use this input :
+5
+5
+0 2
+1 3 5 7 9
+*/
+
+// Solution by self optimal version 1
 void mergeSortArray(vector<int> &arr1, vector<int> &arr2)
 {
-    
+
     int left = arr1.size() - 1, right = 0;
-    while(left > -1 && right < arr2.size()){
-        if(arr1[left] > arr2[right]) {
+    while (left > -1 && right < arr2.size())
+    {
+        if (arr1[left] > arr2[right])
+        {
             swap(arr1[left], arr2[right]);
             left--;
             right++;
         }
-        else break;
+        else
+            break;
     }
-    sort(arr1.begin(), arr1.end());
-    sort(arr2.begin(), arr2.end());
+
+    sort(arr1.begin(), arr1.end()); // sort(arr1, arr1.size())
+    sort(arr2.begin(), arr2.end()); // sort(arr2, arr2.size())
 }
+
+// Optimal solution version 2{shell method} mentioned in the video
 
 int main()
 {
@@ -34,11 +48,13 @@ int main()
     }
 
     mergeSortArray(arr1, arr2);
-    
-    for(int it : arr1){
+
+    for (int it : arr1)
+    {
         cout << it << " ";
     }
-    for(int it : arr2){
+    for (int it : arr2)
+    {
         cout << it << " ";
     }
 }
