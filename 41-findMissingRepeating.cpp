2aa44013a83c77514114ae5a6da8bff75a6e31da
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-11 00:13:19 +0530

    added function

--- 41-findMissingRepeating.cpp
+++ 41-findMissingRepeating.cpp
@@ -4,6 +4,25 @@ using namespace std;
 // brute force method to find missing and repeating number in an array of size n
 void missingAndRepeat(vector<int> &arr)
 {
+    
+    int repeat = -1;
+    int missing = -1;
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < arr.size() - 1; j++)
+        {
+            if (arr[j] == i)
+                count++;
+            if (count == 2)
+                repeat = i;
+            else if (count == 0)
+                missing = i;
+            if (missing == -1 && repeat == -1)
+                break;
+        }
+    }
+    cout << "Repeating and missing numbers are: " << repeat << ", " << missing;
 }
 
 int main()
