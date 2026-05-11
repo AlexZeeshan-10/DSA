5270cf955ff649f14b83b435c7d41eab50f6da4b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-12 01:52:28 +0530

    merge sort added

--- 42-countInversion.cpp
+++ 42-countInversion.cpp
@@ -1,15 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int merge_sort(vector<int>& arr,int l,int h){
-    
+int merge_sort(vector<int> &arr, int l, int h)
+{
+    int cnt = 0;
+    if (l >= h)
+        return 0;
+    int mid = l + (h - l) / 2;
+    cnt += merge_sort(arr, l, mid);
+    cnt += merge_sort(arr, mid + 1, h);
+    cnt += merge(arr, l, mid, h);
+    return cnt;
 }
 
-int inversionCount(vector<int>& arr) {
-    return merge_sort(arr,0,arr.size()-1);
+int inversionCount(vector<int> &arr)
+{
+    return merge_sort(arr, 0, arr.size() - 1);
 }
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n, 0);
@@ -17,6 +27,6 @@ int main(){
     {
         cin >> arr[i];
     }
-    
+
     int ans = inversionCount(arr);
 }
