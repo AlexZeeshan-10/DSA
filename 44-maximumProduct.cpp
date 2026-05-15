d21ff11717160006f8ec98021ab81d698af302e9
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-16 01:06:27 +0530

    Optimal method added

--- 44-maximumProduct.cpp
+++ 44-maximumProduct.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int maxProduct(vector<int> &arr)
 {
-    
+    int maxi = arr[0];
+    int prefix = 1;
+    int suffix = 1;
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (prefix == 0)
+            prefix = 1;
+        if (suffix == 0)
+            suffix = 1;
+        prefix *= arr[i];
+        suffix *= arr[n - i - 1];
+        maxi = max(maxi, max(prefix, suffix));
+    }
+    return maxi;
 }
 
 int main()
