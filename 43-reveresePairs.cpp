4fdf5b1a3963566028ed66ad1fdcb3d65b483fad
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-15 21:42:33 +0530

    brute method added

--- 43-reveresePairs.cpp
+++ 43-reveresePairs.cpp
@@ -1,11 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int countPair(vector<int> &arr)
+{
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[i] > 2 * arr[j])
+                ans++;
+        }
+    }
+    return ans;
+}
 
-
-
-//Main function
-int main(){
+// Main function
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n, 0);
