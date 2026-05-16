7c6406cc5c38dae87e1144c5a3c1414ff31a64ca
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-17 01:27:34 +0530

    iterative binary search added

--- 45-BS_Intro.cpp
+++ 45-BS_Intro.cpp
@@ -1,7 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int BS(vector<int> &arr, int target)
+{
+    int low = 0, high = arr.size() - 1, mid = NULL;
+    while (low <= high)
+    {
+        mid = (low+high) / 2;
+        if(arr[mid] == target) return mid;
+        else if(arr[mid] < target) low = mid+1;
+        else high = mid-1;
+    }
+    return -1;
+}
+
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n, 0);
@@ -9,9 +23,9 @@ int main(){
     {
         cin >> arr[i];
     }
-    
+
     int target;
     cin >> target;
     int ans = BS(arr, target);
-    cout << ans << " ";
+    cout << ans+1 << " ";
 }
