85f24ab5da06d653b7ab6e142cc51f63d3eaa162
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-17 01:07:47 +0530

    main function added

--- 45-BS_Intro.cpp
+++ 45-BS_Intro.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
     
+    int target;
+    cin >> target;
+    int ans = BS(arr, target);
+    cout << ans << " ";
 }
