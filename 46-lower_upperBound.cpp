4129c15d3412c50cfc30606c463fca6345350957
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-19 00:41:30 +0530

    lower bound added

--- 46-lower_upperBound.cpp
+++ 46-lower_upperBound.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int lowerBound(vector<int> &arr, int target){
-    
+    int low = 0, high = arr.size()-1;
+    int ans = arr.size()+1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid] >= target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else low = mid+1;
+    }
+    return ans;
 }
 
 
