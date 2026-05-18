86b9cb1aff0d6435ade075656bae01e1397f6d29
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-19 00:56:26 +0530

    upper bound added

--- 46-lower_upperBound.cpp
+++ 46-lower_upperBound.cpp
@@ -1,16 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int lowerBound(vector<int> &arr, int target){
-    
+int lowerBound(vector<int> &arr, int target)
+{
+
 }
 
-int lowerBound(vector<int> &arr, int target){
-    
+int upperBound(vector<int> &arr, int target)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = arr.size() + 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] > target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
 }
 
-
-int main(){
+int main()
+{
     int n;
     cin >> n;
     vector<int> arr(n, 0);
@@ -18,5 +33,8 @@ int main(){
     {
         cin >> arr[i];
     }
-    
+    int target;
+    cin >> target;
+    int ans = upperBound(arr, target);
+    cout << ans << " ";
 }
