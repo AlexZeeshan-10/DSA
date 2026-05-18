5c679f7b9a4e6ad19fb218d5903528db9c6997fb
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-19 00:11:58 +0530

    beginning vid 46

---
+++ 46-lower_upperBound.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> &arr, int target){
+    
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+    
+}
