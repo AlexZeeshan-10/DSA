db0c446a186eb44f920ae80db457acae673e687d
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-21 02:06:38 +0530

    added optimal code

--- 47-firstAndLastOccur.cpp
+++ 47-firstAndLastOccur.cpp
@@ -14,19 +14,63 @@ vector<int> occurBrute(vector<int> &arr, int target){
     return {i, j};
 }
 
+
+// Lower bound method
+int lowerBound(vector<int> &arr, int target)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = arr.size() + 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] >= target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
+}
+
+// upper bound
+int upperBound(vector<int> &arr, int target)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = arr.size() + 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] > target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
+}
+
 vector<int> occurOptimal(vector<int> &arr, int target){
-    
+    int lb = lowerBound(arr, target);
+    if(lb == arr.size() || arr[lb] != target) return {-1, -1};
+    return {lb, upperBound(arr, target) - 1};
 }
 
 int main(){
     int n;
     cin >> n;
-    vector<int> arr;
+    vector<int> arr(n);
     for (int i = 0; i < arr.size(); i++)
     {
         cin >> arr[i];
     }
     int target;
     cin >> target;
-
+    vector<int> ans = occurOptimal(arr, target);
+    for(auto it : ans){
+        cout << it << " ";
+    }
 }
