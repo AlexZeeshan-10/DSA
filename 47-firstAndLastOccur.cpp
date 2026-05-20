0cbc28de4df359e23782850b33b7af0e24b3af0f
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-21 01:51:06 +0530

    brute method applied

--- 47-firstAndLastOccur.cpp
+++ 47-firstAndLastOccur.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 //brute force method to find the first and last occurence in an array
 vector<int> occurBrute(vector<int> &arr, int target){
-    
+    int i = -1, j = -1;
+    for (int x = 0; x < arr.size(); x++)
+    {
+        if(arr[x] == target){
+            if(i == -1) i = x;
+            j = x;
+        }
+    }
+    return {i, j};
 }
