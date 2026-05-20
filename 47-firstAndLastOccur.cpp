e3af01908a7a672071065dcf74c23117d29a927d
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-05-21 01:50:47 +0530

    intial commit

--- 47-firstAndLastOccur.cpp
+++ 47-firstAndLastOccur.cpp
@@ -1,3 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//brute force method to find the first and last occurence in an array
+vector<int> occurBrute(vector<int> &arr, int target){
+    
+}
