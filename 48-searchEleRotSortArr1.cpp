a611e43df3d7f7e36033ea6a21334ffb28b97e76
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-09 19:56:38 +0530

    main function

--- 48-searchEleRotSortArr1.cpp
+++ 48-searchEleRotSortArr1.cpp
@@ -1,6 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+
 
 int main(){
-    
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        std::cin >> arr[i];
+    }
 }
