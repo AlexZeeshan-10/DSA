c1897ef5ab9d7eaaa90091c295243ec7a954d747
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-10 18:41:38 +0530

    function added

--- 49-searchEleRotSortArr2.cpp
+++ 49-searchEleRotSortArr2.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <vector>
 
-int searchRotArr(std::vector<int> &arr, int target)
+bool searchRotArr(std::vector<int> &arr, int target)
 {
     int low = 0, high = arr.size() - 1;
-    while (low <= high)
-    {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == target)
-            return mid;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-        // left sorted
-        if (arr[low] <= arr[mid])
-        {
-            if (arr[low] <= target && target <= arr[mid])
-                high = mid - 1;
-            else
-                low = mid + 1;
+            if (arr[mid] == target)
+                return true;
+            
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+            // left sorted
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // right sorted
+            else {
+                if (arr[mid] <= target && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
         }
-        // right sorted
-        else
-        {
-            if (arr[mid] <= target && target <= arr[high])
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-    }
-    return -1;
+        return false;
 }
 
 int main()
@@ -42,4 +46,5 @@ int main()
     int target;
     std::cin >> target;
     bool ans = searchRotArr(arr, target);
+    std::cout << ans << std::endl;
 }
