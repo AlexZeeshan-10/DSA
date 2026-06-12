1c50acd53e96a441c93ffbe0651afa102bced192
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-12 20:10:40 +0530

    beginning vid 52

---
+++ 52-singleEleSortArr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        std::cin >> arr[i];
+    }
+    
+}
