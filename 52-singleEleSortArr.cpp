5b81439cfb8c2b1fb722056be8afd638cd828ffc
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-12 20:21:50 +0530

    brute function added

--- 52-singleEleSortArr.cpp
+++ 52-singleEleSortArr.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <vector>
 
-int bruteFindEle(std::vector<int> &arr){
+int bruteFindEle(std::vector<int> &arr)
+{
     int n = arr.size();
-    
-        return -1;
+    if (n == 1)
+        return arr[0];
+    if (arr[0] != arr[1])
+        return arr[0];
+    if (arr[n - 1] != arr[n - 2])
+        return arr[n - 1];
+    for (size_t i = 1; i < n - 1; i++)
+    {
+        if (arr[i - 1] != arr[i] && arr[i + 1] != arr[i])
+            return arr[i];
+    }
+    return -1;
 }
 
-int main(){
+int main()
+{
     int n;
     std::cin >> n;
     std::vector<int> arr(n);
