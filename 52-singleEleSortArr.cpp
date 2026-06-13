523c3f166fd64853fb978eec4dae55ef23ff275a
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-13 16:03:20 +0530

    optimal feature added

--- 52-singleEleSortArr.cpp
+++ 52-singleEleSortArr.cpp
@@ -18,7 +18,8 @@ int bruteFindEle(std::vector<int> &arr)
     return -1;
 }
 
-int optFindEle(std::vector<int> &arr){
+int optFindEle(std::vector<int> &arr)
+{
     int n = arr.size();
     if (n == 1)
         return arr[0];
@@ -26,6 +27,22 @@ int optFindEle(std::vector<int> &arr){
         return arr[0];
     if (arr[n - 1] != arr[n - 2])
         return arr[n - 1];
+    int low = 1, high = n - 2;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
+            return arr[mid];
+
+        // Pointing at the left half
+        if ((mid % 2 == 1 && arr[mid - 1] == arr[mid]) || (mid % 2 == 0 && arr[mid] == arr[mid + 1]))
+            low = mid + 1;
+
+        // Pointing at the right half
+        else
+            high = mid - 1;
+    }
+    return -1;
 }
 
 int main()
