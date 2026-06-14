e846c2c529736fc7862ef39117d6533b697e39f9
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-14 17:52:20 +0530

    brute approach added

--- 53-findPeakEle.cpp
+++ 53-findPeakEle.cpp
@@ -3,7 +3,16 @@
 
 int findPeakElement(std::vector<int> &arr)
 {
-    
+    int n = arr.size();
+    /* This solution would require just one iteration.
+        The readability gets quite complex. */
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        if ((i == 0 || arr[i - 1] < arr[i]) &&
+            ((i == n - 1) || arr[i] > arr[i + 1]))
+            return i;
+    }
+    return -1;
 }
 
 int main()
