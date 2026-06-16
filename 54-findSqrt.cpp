3a52e935ddad7c5877dc4c097b7bb9043dd88bdf
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-16 18:26:11 +0530

    function added

--- 54-findSqrt.cpp
+++ 54-findSqrt.cpp
@@ -4,7 +4,16 @@
 #include <algorithm>
 
 int sqrtBS(int x){
-    
+    int low = 0, high = x;
+    while (low <= high){
+        long long mid = low + (high - low) / 2;
+        long long val = mid * mid;
+        if(val <= x)
+            low = mid + 1;
+        else 
+            high = mid - 1;
+    }
+    return high;
 }
 
 int main(){
