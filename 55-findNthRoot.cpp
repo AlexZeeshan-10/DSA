c158557851e8d9710da2f3828199eecf2d732885
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-17 16:09:12 +0530

    creating branch

--- 55-findNthRoot.cpp
+++ 55-findNthRoot.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 int main()
 {
     int n;
     std::cin >> n;
-    std::vector<int> arr(n);
-    for (size_t i = 0; i++ < arr.size();)
-    {
-        std::cin >> arr[i];
-    }
+    int ans = rootBrute(n);
+    std::cout << ans;
 }
