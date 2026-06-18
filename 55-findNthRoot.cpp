45288889c16acaad52eaaf5c87324e2f73f30888
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-18 16:08:38 +0530

    added brute approach [skip ci]

--- 55-findNthRoot.cpp
+++ 55-findNthRoot.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <algorithm>
 
-int rootBrute(int n){
-    int ans = -1;
+long long mul(int i, int n, int m)
+{
+    long long ans = 1;
+    for (int j = 1; j <= n; j++)
+    {
+        ans *= i;
+        if (ans > m) return ans; // Break early to prevent integer overflow!
+    }
+    return ans;
+}
+
+int rootBrute(int m, int n)
+{
+    for (int i = 1; i <= m; i++)
+    {
+        long long val = mul(i, n, m);
+        if (val == m)
+            return i;
+        else if (val > m)
+            break;
+    }
+    return -1;
 }
 
 int main()
 {
-    int n;
+    int m, n;
+    std::cin >> m;
     std::cin >> n;
-    int ans = rootBrute(n);
+    int ans = rootBrute(m, n);
     std::cout << ans;
 }
