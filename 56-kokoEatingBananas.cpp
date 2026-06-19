93577c5914ec2c67b8073874333c45c1daa2b5f4
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-19 18:00:36 +0530

    function added

--- 56-kokoEatingBananas.cpp
+++ 56-kokoEatingBananas.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+
+int findMax(std::vector<int> &arr){
+  int maxi = INT_MAX;
+  for(auto it : arr){
+    maxi = std::max(maxi, it);
+  }
+  return maxi;
+}
+
+int calculateTotalHours(std::vector<int> &arr, int hourly){
+  int totalH = 0;
+  for(size_t i = 0; i < arr.size(); ++i){
+    totalH += std::ceil((double) arr[i] / (double) hourly);
+  }
+  return totalH;
+}
+
+int kokoEatBanana(std::vector<int> &arr, int deadLine){
+  int low = 1, high = findMax(arr);
+  while (low <= high)
+  {
+    int mid = low + (high - low) / 2;
+    int totatH = calculateTotalHours(arr, mid);
+    if (totatH <= deadLine)
+    {
+      high = mid - 1;
+    }
+    else 
+      low = mid + 1;
+  }
+  return low;
+}
 
 int main(){
-    int n;
-    std::cin >> n;
-    std::vector<int> arr(n);
-    for (size_t i = 0; i++ < arr.size();)
-    {
-        std::cin >> arr[i];
-    }
-    int deadLine;
-    std::cin >> deadLine;
-    int ans(arr, deadLine);
-    std::cout << ans << std::endl;
+  int n;
+  std::cin >> n;
+  std::vector<int> arr(n);
+  for (size_t i = 0; i++ < arr.size();)
+  {
+    std::cin >> arr[i];
+  }
+  int deadLine;
+  std::cin >> deadLine;
+  int ans = kokoEatBanana(arr, deadLine);
+  std::cout << ans << std::endl;
 }
