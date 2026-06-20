cda2f53b9cfb4949acf25f92be62a51cf117afdd
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-20 16:31:35 +0530

    main function added

--- 57-minDaysForMBouq.cpp
+++ 57-minDaysForMBouq.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 int main(){
-    
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for (size_t i = 0; i < arr.size(); ++i)
+    {
+        std::cin >> arr[i];
+    }
     
 }
