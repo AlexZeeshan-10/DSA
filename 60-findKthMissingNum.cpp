b849a2cb240857d36f4161b294377f147eb83c90
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-24 17:40:13 +0530

    added binary search function

--- 60-findKthMissingNum.cpp
+++ 60-findKthMissingNum.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <vector>
 
-int findKthNumBS(std::vector<int> &arr, int k){
-    
+int findKthNumBS(std::vector<int> &arr, int k)
+{
+    int low = 0, high = arr.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int missing = arr[mid] - (mid + 1);
+        if (missing < k)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return arr[high] + k - (arr[high] - high - 1);
 }
 
-int main(){
+int main()
+{
     int n;
     std::cin >> n;
     std::vector<int> arr(n);
@@ -14,10 +26,10 @@ int main(){
     {
         std::cin >> arr[i];
     }
-    
+
     int k;
     std::cin >> k;
 
-    int ans = findKthNum(arr, k);
+    int ans = findKthNumBS(arr, k);
     std::cout << ans << std::endl;
 }
