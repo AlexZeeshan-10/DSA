49bfd2cf7668131c9dda5e8d4a538dc42fe1fa9c
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-25 18:34:13 +0530

    initial BS branch commit

--- 62-allocateBooks.cpp
+++ 62-allocateBooks.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <vector>
 
+int cntStudent(std::vector<int> &arr, int pages){
+    int students = 1;
+    long long pagesStudent = 0;
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        if(pagesStudent + arr[i] <= pages)
+        {
+            pagesStudent += arr[i];
+        }
+        else
+        {
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
 int main()
 {
     int n;
