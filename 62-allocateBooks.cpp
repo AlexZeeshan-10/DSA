57e91fd8010c1131cfa3d3d252e0b348907264ab
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-06-25 18:30:20 +0530

    added function to compare

--- 62-allocateBooks.cpp
+++ 62-allocateBooks.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
+
+
 
 int findPagesBrute(std::vector<int> &arr, int student)
 {
     if (arr.size() < student)
         return -1;
+
+    int low = *std::max_element(arr.begin(), arr.end());
+    int high = std::accumulate(arr.begin(), arr.end(), 0);
+    for (int pages = low; pages <= high; pages++)
+    {
+        int studentsNeeded = cntStudent(arr, pages);
+        if(studentsNeeded <= student)
+            return pages;
+    }
+    return -1;
 }
 
 int main()
