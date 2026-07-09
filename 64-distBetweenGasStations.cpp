61f77bd018b1e08a4e9c5e323a257d861b7441da
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-09 18:10:40 +0530

    brute force code added

--- 64-distBetweenGasStations.cpp
+++ 64-distBetweenGasStations.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 double minimiseMaxDistanceBrute(std::vector<int> &arr, int k){
+    std::vector<int> howMany(arr.size() - 1, 0);
+    // Loop k times to place k gas stations one by one.
+    for (int gasStations = 1; gasStations <= k; gasStations++)
+    {
+        long double maxSection = -1;
+        int maxInd = -1;
+        // Find the section with the largest distance to place the next station.
+        for (size_t i = 0; i < arr.size() - 1; i++)
+        {
+            long double diff = (arr[i + 1] - arr[i]);
+            long double sectionLength = diff / (long double)(howMany[i] + 1);
+            if (sectionLength > maxSection)
+            {
+                maxSection = sectionLength;
+                maxInd = i;
+            }
+        }
+        // Place the new station in the section that had the maximum distance.
+        howMany[maxInd]++;
+    }
 
+    // After placing all stations, find the final maximum distance.
+    long double maxAns = -1;
+    for (size_t i = 0; i < arr.size() - 1; i++)
+    {
+        long double diff = (arr[i+1] - arr[i]);
+        long double sectionLength = diff / (long double) (howMany[i] + 1);
+        maxAns = std::max(maxAns, sectionLength);
+    }
+    return maxAns;
 }
 
 int main(){
@@ -17,6 +47,6 @@ int main(){
     int k;
     std::cin >> k;
 
-    int ans = minimiseMaxDistanceBrute(arr, k);
+    double ans = minimiseMaxDistanceBrute(arr, k);
     std::cout << ans << std::endl;
 }
