a85ce3f63a39015fbdc5dcaa53a0860966793da5
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-09 17:40:49 +0530

    added main function

--- 64-distBetweenGasStations.cpp
+++ 64-distBetweenGasStations.cpp
@@ -2,5 +2,16 @@
 #include <vector>
 
 int main(){
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        std::cin >> arr[i];
+    }
+    
+    int k;
+    std::cin >> k;
+
     
 }
