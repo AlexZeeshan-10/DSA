d91d39017da39deb8124eaa042524b6ef59bbd32
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-10 18:12:37 +0530

    added main function

--- 65-medianTwoSortedArrDiffSize.cpp
+++ 65-medianTwoSortedArrDiffSize.cpp
@@ -2,7 +2,22 @@
 #include <vector>
 
 int main(){
-    int n;
-    std::cin >> n;
+    int n1;
+    std::cin >> n1;
+    std::vector<int> arr1(n1);
+    for (size_t i = 0; i < n1; i++)
+    {
+        std::cin >> arr1[i];
+    }
     
+    int n2;
+    std::cin >> n2;
+    std::vector<int> arr2(n2);
+    for (size_t i = 0; i < n2; i++)
+    {
+        std::cin >> arr2[i];
+    }
+    
+    double ans = median(arr1, arr2);
+    std::cout << ans << std::endl;
 }
