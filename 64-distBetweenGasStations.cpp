297d3dd0d7c1279b246a7c53e09838c90a24c9ce
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-10 17:00:37 +0530

    better code added for vid 64

--- 64-distBetweenGasStations.cpp
+++ 64-distBetweenGasStations.cpp
@@ -39,7 +39,21 @@ double minimiseMaxDistanceBrute(std::vector<int> &arr, int k)
 
 double minimiseMaxDistanceBetter(std::vector<int> &arr, int k)
 {
-    
+    std::vector<int> howMany(arr.size() - 1, 0);
+    std::priority_queue<std::pair<long double, int>> pq;
+    for(size_t i = 0; i < arr.size()-1; i++){
+        pq.push({arr[i+1] - arr[i], i});
+    }
+
+    for (size_t gasStation = 0; gasStation <= k; gasStation++)
+    {
+        auto tp = pq.top(); pq.pop();
+        int secInd = tp.second;
+        howMany[secInd]++;
+        long double iniDiff = arr[secInd + 1] - arr[secInd];
+        long double newSecLen = iniDiff / (long double)(howMany[secInd] + 1);
+        }
+    return pq.top().first;
 }
 
 int main()
