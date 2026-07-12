ea32ce31be936eb2cd788e34f5d4f0eeb7ec757f
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 18:02:04 +0530

    main function added

--- 67-kthElementInTwoSortedArr.cpp
+++ 67-kthElementInTwoSortedArr.cpp
@@ -2,5 +2,26 @@
 #include <vector>
 
 int main(){
+    int n1;
+    std::cin >> n1;
+    std::vector<int> arr1(n1);
+    for (size_t i = 0; i < n1; i++)
+    {
+        std::cin >> arr1[i];
+    }
+
+    int n2;
+    std::cin >> n2;
+
+    std::vector<int> arr2(n2);
+    for (size_t i = 0; i < n2; i++)
+    {
+        std::cin >> arr2[i];
+    }
     
+    int k;
+    std::cin >> k;
+
+    int ans = kthElement(arr1, arr2, k);
+    std::cout << ans << std::endl;
 }
