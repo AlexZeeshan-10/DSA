9edeb45a3d940164c9916a000d3e7f0b317df2ba
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 20:21:01 +0530

    added 2d to 1d matrix conversion approach

--- 4. Binary Search/69-searchInSortedMatrix.cpp
+++ 4. Binary Search/69-searchInSortedMatrix.cpp
@@ -44,6 +44,31 @@ bool searchMatrixBS(std::vector<std::vector<int>> &matrix, int target)
     return false;
 }
 
+bool searchMatrixOptimal1D(std::vector<std::vector<int>> &matrix, int target)
+{
+    int n = matrix.size();
+    if (n == 0)
+        return false;
+    int m = matrix[0].size();
+
+    int low = 0, high = (n * m) - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int row = mid / m;
+        int col = mid % m;
+
+        if (matrix[row][col] == target)
+            return true;
+        else if (matrix[row][col] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return false;
+}
+
 int main()
 {
     int row, column;
@@ -63,7 +88,6 @@ int main()
     int target;
     std::cin >> target;
 
-    bool ans = searchMatrixBrute(mat, target);
-    // std::cout << ans << std::endl;
+    bool ans = searchMatrixOptimal1D(mat, target);
     (ans == 1) ? std::cout << "True" : std::cout << "False";
 }
