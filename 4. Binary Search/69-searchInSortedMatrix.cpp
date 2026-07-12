d56ca234fe221a4814e9b6b23fb45187b41a71ce
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 19:26:01 +0530

    fetching matrix function added into main

--- 4. Binary Search/69-searchInSortedMatrix.cpp
+++ 4. Binary Search/69-searchInSortedMatrix.cpp
@@ -2,5 +2,17 @@
 #include <vector>
 
 int main(){
-    
+    int row, column;
+    std::cin >> row;
+    std::cin >> column;
+
+    std::vector<std::vector<int>> mat(row, std::vector<int>(column));
+
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < column; j++)
+        {
+            std::cin >> mat[i][j];
+        }
+    }
 }
