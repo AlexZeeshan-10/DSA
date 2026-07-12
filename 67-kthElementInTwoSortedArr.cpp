8c7e5d2e66afe026396f686a1be8868e51195fef
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 18:08:46 +0530

    added optimal function

--- 67-kthElementInTwoSortedArr.cpp
+++ 67-kthElementInTwoSortedArr.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
 
-int kthElement(std::vector<int> &arr1, std::vector<int> &arr2, int k){
+int kthElement(std::vector<int> &arr1, std::vector<int> &arr2, int k)
+{
     if (arr1.size() > arr2.size())
-        return medianBS(arr2, arr1);
-    int low = 0, high = arr1.size();
-    int left = (arr1.size() + arr2.size() + 1) / 2;
-    int n = arr1.size() + arr2.size();
+        return kthElement(arr2, arr1, k);
+    int n1 = arr1.size(), n2 = arr2.size();
+    // Ensure subtraction is done with signed integers to avoid underflow wrap-around.
+    int low = std::max(0, k - n2), high = std::min(k, n1);
+    int left = k;
     while (low <= high)
     {
         int mid1 = (low + high) >> 1;
         int mid2 = left - mid1;
         int l1 = INT_MIN, l2 = INT_MIN;
         int r1 = INT_MAX, r2 = INT_MAX;
-        r1 = (mid1 < arr1.size()) ? arr1[mid1] : r1;
-        r2 = (mid2 < arr2.size()) ? arr2[mid2] : r2;
+        r1 = (mid1 < n1) ? arr1[mid1] : r1;
+        r2 = (mid2 < n2) ? arr2[mid2] : r2;
         l1 = (mid1 - 1 >= 0) ? arr1[mid1 - 1] : l1;
         l2 = (mid2 - 1 >= 0) ? arr2[mid2 - 1] : l2;
         if (l1 <= r2 && l2 <= r1)
         {
-            if (n % 2 == 1)
-                return std::max(l1, l2);
-            return ((double)(std::max(l1, l2) + std::min(r1, r2))) / 2.0;
+            return std::max(l1, l2);
         }
         else if (l1 > r2)
             high = mid1 - 1;
@@ -31,7 +33,8 @@ int kthElement(std::vector<int> &arr1, std::vector<int> &arr2, int k){
     return 0;
 }
 
-int main(){
+int main()
+{
     int n1;
     std::cin >> n1;
     std::vector<int> arr1(n1);
@@ -48,7 +51,7 @@ int main(){
     {
         std::cin >> arr2[i];
     }
-    
+
     int k;
     std::cin >> k;
 
