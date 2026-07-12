7e715bbfc78fbd257a336ddf4763a3ed0bf5d74b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 19:14:20 +0530

    function of lower bound added in branch

--- 4. Binary Search/68-rowWithMaxOnes.cpp
+++ 4. Binary Search/68-rowWithMaxOnes.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <vector>
 
+int lowerBound(vector<int> &arr, int target)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = arr.size() + 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] >= target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
+}
+
 int main(){
     int row, column;
     std::cin >> row;
