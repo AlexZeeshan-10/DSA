d008ab5218e3aff9f7884311c138747b8331cd41
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 19:40:23 +0530

    brute code added

--- 4. Binary Search/69-searchInSortedMatrix.cpp
+++ 4. Binary Search/69-searchInSortedMatrix.cpp
@@ -2,7 +2,13 @@
 #include <vector>
 
 bool searchMatrixBrute(std::vector<std::vector<int>>, int target){
-    
+    for(size_t i = 0; i<matrix.size(); ++i){
+            for(size_t j = 0; j < matrix[0].size(); ++j){
+                if (matrix [i][j] == target)
+                    return true;
+            }
+        }
+        return false;
 }
 
 int main(){
