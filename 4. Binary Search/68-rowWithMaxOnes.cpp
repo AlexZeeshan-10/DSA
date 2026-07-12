e2e3dc01464188d96b0bfeecd0b9824355adf6a4
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 19:19:55 +0530

    added function to count max ones

--- 4. Binary Search/68-rowWithMaxOnes.cpp
+++ 4. Binary Search/68-rowWithMaxOnes.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-int lowerBound(vector<int> &arr, int target)
+int lowerBound(std::vector<int> &arr, int target)
 {
     int low = 0, high = arr.size() - 1;
     int ans = arr.size() + 1;
@@ -17,6 +17,17 @@ int lowerBound(vector<int> &arr, int target)
             low = mid + 1;
     }
     return ans;
+}
+
+int rowWithMax1s(std::vector<std::vector<int>> &mat, int n, int m){
+    int cnt_max = 0;
+    int index = -1;
+    for (size_t i = 0; i < n; i++)
+    {
+        int cnt_ones = m - lowerBound(mat[i], 1);
+        (cnt_ones > cnt_max) ? cnt_max = cnt_ones, index = i : 0;
+    }
+    return index;
 }
 
 int main(){
