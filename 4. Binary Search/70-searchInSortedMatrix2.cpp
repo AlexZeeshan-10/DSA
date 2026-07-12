0a43bf77c9360cefb342279b845a032bf352c532
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 20:56:53 +0530

    added main function

--- 4. Binary Search/70-searchInSortedMatrix2.cpp
+++ 4. Binary Search/70-searchInSortedMatrix2.cpp
@@ -2,5 +2,20 @@
 #include <vector>
 
 int main(){
-    
+    int row, column;
+    std::cin >> row;
+    std::cin >> column;
+
+    std::vector<std::vector<int>> mat(row, std::vector<int>(column));
+
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < column; j++)
+        {
+            std::cin >> mat[i][j];
+        }
+    }
+
+    int target;
+    std::cin >> target;
 }
