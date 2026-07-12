6e010844fbf454a05d6a086e1153ef1af341795e
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 19:54:54 +0530

    added binary search

--- 4. Binary Search/69-searchInSortedMatrix.cpp
+++ 4. Binary Search/69-searchInSortedMatrix.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <vector>
 
+bool binarySearch(std::vector<int> &arr, int target){
+    int low = 0, high = arr.size()-1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == target) return true;
+        else if(arr[mid] > target) high = mid - 1;
+        else low = mid + 1;
+    }
+    return false;
+}
+
 bool searchMatrixBS(std::vector<std::vector<int>> &matrix, int target){
     int n = matrix.size();
     int m = matrix[0].size();
     for(size_t i = 0; i<matrix.size(); ++i){
-            if(matrix[i][0] <= target && matrix[i][m-1]){
+            if(matrix[i][0] <= target && target <= matrix[i][m-1]){
                 return binarySearch(matrix[i], target);
             }
         }
@@ -31,5 +43,6 @@ int main(){
     std::cin >> target;
     
     bool ans = searchMatrixBS(mat, target);
-    std::cout << ans << std::endl;
+    // std::cout << ans << std::endl;
+    (ans == 1) ? std::cout << "True" : std::cout << "False";
 }
