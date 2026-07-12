855ce420d5e0e301e277bbcfd41b2a4b13d26ff7
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-12 18:04:43 +0530

    function added

--- 67-kthElementInTwoSortedArr.cpp
+++ 67-kthElementInTwoSortedArr.cpp
@@ -2,7 +2,33 @@
 #include <vector>
 
 int kthElement(std::vector<int> &arr1, std::vector<int> &arr2, int k){
-    
+    if (arr1.size() > arr2.size())
+        return medianBS(arr2, arr1);
+    int low = 0, high = arr1.size();
+    int left = (arr1.size() + arr2.size() + 1) / 2;
+    int n = arr1.size() + arr2.size();
+    while (low <= high)
+    {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        r1 = (mid1 < arr1.size()) ? arr1[mid1] : r1;
+        r2 = (mid2 < arr2.size()) ? arr2[mid2] : r2;
+        l1 = (mid1 - 1 >= 0) ? arr1[mid1 - 1] : l1;
+        l2 = (mid2 - 1 >= 0) ? arr2[mid2 - 1] : l2;
+        if (l1 <= r2 && l2 <= r1)
+        {
+            if (n % 2 == 1)
+                return std::max(l1, l2);
+            return ((double)(std::max(l1, l2) + std::min(r1, r2))) / 2.0;
+        }
+        else if (l1 > r2)
+            high = mid1 - 1;
+        else
+            low = mid1 + 1;
+    }
+    return 0;
 }
 
 int main(){
