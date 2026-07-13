8912dc3812a12ca2ea315cbe79555fad163d47d7
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-13 17:57:48 +0530

    added code to fetch matrix input in main

--- 4. Binary Search/71-findPeakElement2.cpp
+++ 4. Binary Search/71-findPeakElement2.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 
 int main(){
-    
+    int row, column;
+    std::cin >> row;
+    std::cin >> column;
+    std::vector<std::vector<int>> mat(row, std::vector<int>(column));
+    for (size_t i = 0; i < row; ++i)
+    {
+        for (size_t j = 0; j < column; ++j)
+        {
+            std::cin >> mat[i][j];
+        }
+    }
     
 }
