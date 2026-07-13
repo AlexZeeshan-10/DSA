58d734f77486e0f5e0f413b0812206a30cbd4cd1
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-13 19:28:36 +0530

    inital branch commit optimal

--- 4. Binary Search/71-findPeakElement2.cpp
+++ 4. Binary Search/71-findPeakElement2.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> findPeakGridBS(std::vector<std::vector<int>> &mat){
+    int n = mat.size(), m = mat[0].size();
+    int low = 0, high = m - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int maxRowIndex = findMaxIndex(mat, n, m, mid);
+        
+    }
+    
+}
+
 int main(){
     int row, column;
     std::cin >> row;
@@ -14,6 +26,6 @@ int main(){
         }
     }
     
-    std::vector<int> ans = findPeakGrid(mat);
+    std::vector<int> ans = findPeakGridBS(mat);
     std::cout << ans[0] << " " << ans[1] << std::endl;
 }
