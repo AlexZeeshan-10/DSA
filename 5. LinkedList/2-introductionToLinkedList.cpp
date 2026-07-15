67e3e297dfc9027f28c1291d1632183759542a14
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-15 18:11:08 +0530

    add: added a function to count the elements in LL

--- 5. LinkedList/2-introductionToLinkedList.cpp
+++ 5. LinkedList/2-introductionToLinkedList.cpp
@@ -33,19 +33,40 @@ Node* convertArrayToLinkedList(std::vector<int> &arr){
     return head;
 }
 
+int lengthOfLinkedList(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while (temp)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
 int main()
 {
+    // basics
     // vector<int> arr = {2, 5, 8, 7};
     // Node *y = new Node(arr[0], nullptr);
     // cout << y->data;
 
+
+    // Traversing a array into a linkedlist
+
+    // std::vector<int> arr = {12, 5, 8, 7};
+    // Node* head = convertArrayToLinkedList(arr);
+    // Node* temp = head;
+    // while (temp)
+    // {
+    //     std::cout << temp->data << " -> ";
+    //     temp = temp->next;
+    // }
+    // std::cout << "nullptr" << std::endl;
+
+
+    //length count of a linked list
     std::vector<int> arr = {12, 5, 8, 7};
     Node* head = convertArrayToLinkedList(arr);
-    Node* temp = head;
-    while (temp)
-    {
-        std::cout << temp->data << " -> ";
-        temp = temp->next;
-    }
-    std::cout << "nullptr" << std::endl;
+    std::cout << lengthOfLinkedList(head);
 }
