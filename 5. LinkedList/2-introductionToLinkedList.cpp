d47054fb22577f749559846fcab3e6e88edf2556
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-15 16:46:33 +0530

    data type added

--- 5. LinkedList/2-introductionToLinkedList.cpp
+++ 5. LinkedList/2-introductionToLinkedList.cpp
@@ -6,8 +6,12 @@ struct Node {
     int data;
     Node* next;
 
-    
-}
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
 
 int main(){
     vector<int> arr = {2, 5, 8, 7};
