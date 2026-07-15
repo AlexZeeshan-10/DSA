0a9c894b7a651d2fd33aadf9283a3a6e9a30cd10
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-15 16:46:56 +0530

    file: formatted

--- 5. LinkedList/2-introductionToLinkedList.cpp
+++ 5. LinkedList/2-introductionToLinkedList.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    public:
+struct Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    public:
-    Node(int data1, Node* next1){
+public:
+    Node(int data1, Node *next1)
+    {
         data = data1;
         next = next1;
     }
 };
 
-int main(){
+int main()
+{
     vector<int> arr = {2, 5, 8, 7};
-    Node* y = new Node(arr[0], nullptr);
+    Node *y = new Node(arr[0], nullptr);
     cout << y;
 }
