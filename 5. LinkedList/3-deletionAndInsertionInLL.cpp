e90491c87b30aeeae9f82100f20a2926b913d33e
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-17 16:09:55 +0530

    add: class node

--- 5. LinkedList/3-deletionAndInsertionInLL.cpp
+++ 5. LinkedList/3-deletionAndInsertionInLL.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+class Node
+{
+public:
+    int data;
+    Node *next;
 
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
 
 int main()
 {
