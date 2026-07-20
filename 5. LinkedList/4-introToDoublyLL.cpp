cda625149ef286e97502d9dd8a620c8ff43a1af9
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-20 18:49:19 +0530

    add: function added for doubly linked list

--- 5. LinkedList/4-introToDoublyLL.cpp
+++ 5. LinkedList/4-introToDoublyLL.cpp
@@ -6,11 +6,12 @@ class Node
 public:
     int data;
     Node *next;
-    Node(int data1, Node *next1)
-        : data(data1), next(next1) {}
+    Node *prev;
+    Node(int data1, Node *next1, Node *prev1)
+        : data(data1), next(next1), prev(prev1) {}
 
     Node(int data1)
-        : data(data1), next(nullptr) {}
+        : data(data1), next(nullptr), prev(nullptr) {}
 };
 
 Node *convertArrayToLL(std::vector<int> &arr)
@@ -26,9 +27,20 @@ Node *convertArrayToLL(std::vector<int> &arr)
     return head;
 }
 
-Node *convertToDoublyLL(Node *head)
+Node *convertToDoublyLL(std::vector<int> &arr)
 {
-    
+    if (arr.empty())
+        return nullptr;
+
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+    for (int i = 1; i < arr.size(); ++i)
+    {
+        Node *temp = new Node(arr[i], nullptr, prev);
+        prev -> next = temp;
+        prev = temp;
+    }
+    return head;
 }
 
 int main()
@@ -41,15 +53,24 @@ int main()
         std::cin >> arr[i];
     }
 
-    Node *head = convertArrayToLL(arr);
+    Node *head;
 
-    head = convertToDoublyLL(head);
+    // head = convertArrayToLL(arr);
+
+    head = convertToDoublyLL(arr);
+
+    if (head == nullptr)
+    {
+        std::cout << "nullptr" << std::endl;
+        return 0;
+    }
 
     Node *temp = head;
-    while (temp)
+    std::cout << "nullptr <-> ";
+    while (temp != nullptr)
     {
-        std::cout << temp->data << " -> ";
+        std::cout << temp->data << " <-> ";
         temp = temp->next;
     }
-    std::cout << " nullptr ";
+    std::cout << "nullptr" << std::endl;
 }
