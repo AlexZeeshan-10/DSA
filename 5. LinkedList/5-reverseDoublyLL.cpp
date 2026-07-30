22cbcd5940b1460aad13bc1fb5ddad40c2ae6db5
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-07-30 23:07:36 +0530

    add: convert array to Doubly LL and main function

--- 5. LinkedList/5-reverseDoublyLL.cpp
+++ 5. LinkedList/5-reverseDoublyLL.cpp
@@ -13,3 +13,30 @@ public:
     Node(int data1)
         : data(data1), next(nullptr), prev(nullptr) {}
 };
+
+
+Node *convertToDoublyLL(std::vector<int> &arr){
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (size_t i = 0; i < arr.size(); ++i)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for (size_t i = 0; i < arr.size(); ++i)
+    {
+        std::cin >> arr[i];
+    }
+    
+    Node *head = convertToDoublyLL(arr);
+
+    return 0;
+}
