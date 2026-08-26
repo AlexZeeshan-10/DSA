753c1165e2c8ce7508c64bde07b0a004572066e1
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-26 23:29:07 +0530

    edit: features for addition

--- 5. LinkedList/6-addTwoNumInLL.cpp
+++ 5. LinkedList/6-addTwoNumInLL.cpp
@@ -36,6 +36,40 @@ Node *CreateLL(int x)
     return head;
 }
 
+Node *addTwoNumbers(Node *l1, Node *l2)
+{
+    Node *dummyNode = new Node(-1);
+    Node *current = dummyNode;
+    Node *t1 = l1;
+    Node *t2 = l2;
+    int carry = 0;
+    while (t1 != nullptr || t2 != nullptr)
+    {
+        int sum = carry;
+        if (t1)
+            sum += t1->data;
+        if (t2)
+            sum += t2->data;
+        Node *sumNode = new Node(sum % 10);
+        carry = sum / 10;
+        current->next = sumNode;
+        current = current->next;
+
+        if (t1)
+            t1 = t1->next;
+        if (t2)
+            t2 = t2->next;
+    }
+    if (carry)
+    {
+        Node *leftCarry = new Node(carry);
+        current->next = leftCarry;
+    }
+
+    return dummyNode->next;
+    delete dummyNode;
+}
+
 int main()
 {
     int n1, n2;
