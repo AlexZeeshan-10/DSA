2b479c78404d38b058cfd925c6d3887b505f6768
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-28 20:44:37 +0530

    add: defined Node class

---
+++ 5. LinkedList/7-oddEvenLL.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+        :data(data1), next(next1){}
+
+    Node(int data1)
+        :data(data1), next(nullptr){}
+};
+
