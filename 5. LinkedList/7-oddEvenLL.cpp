6ba9c7a984a5ec07bb4a710c8c9b4372dc71780b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-29 16:29:36 +0530

    edit: function defined for odd even

--- 5. LinkedList/7-oddEvenLL.cpp
+++ 5. LinkedList/7-oddEvenLL.cpp
@@ -28,10 +28,24 @@ Node *convertArrToLL(const std::vector<int> &arr)
     return head;
 }
 
-Node *oddEven(Node *head){
+Node *oddEven(Node *head)
+{
     std::vector<int> arr;
     Node *temp = head;
-    
+    while (temp != nullptr && temp->next != nullptr)
+    {
+        arr.push_back(temp->data);
+        temp = temp->next->next;
+    }
+    temp = head->next;
+    while (temp != nullptr && temp->next != nullptr)
+    {
+        arr.push_back(temp->data);
+        temp = temp->next->next;
+    }
+
+    arr.push_back(temp->data);
+    return convertArrToLL(arr);
 }
 
 int main()
@@ -61,7 +75,9 @@ int main()
         }
     }
 
-    Node *temp = head;
+    Node *head1 = oddEven(head);
+
+    Node *temp = head1;
 
     while (temp)
     {
