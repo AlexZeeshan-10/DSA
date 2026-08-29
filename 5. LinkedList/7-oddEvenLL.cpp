c81890d7c86434b37726f104be78f8f34141c01c
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-29 15:50:31 +0530

    edit: main funciton

--- 5. LinkedList/7-oddEvenLL.cpp
+++ 5. LinkedList/7-oddEvenLL.cpp
@@ -15,6 +15,15 @@ public:
         : data(data1), next(nullptr) {}
 };
 
-int main(){
-    
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for (size_t i = 0; i < n; ++i)
+    {
+        std::cin >> arr[i];
+    }
 }
