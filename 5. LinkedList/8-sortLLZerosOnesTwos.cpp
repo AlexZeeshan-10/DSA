b906e97b4b103677f75bacbb97a825b89f709ca3
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-30 18:05:03 +0530

    edit: partial features added

--- 5. LinkedList/8-sortLLZerosOnesTwos.cpp
+++ 5. LinkedList/8-sortLLZerosOnesTwos.cpp
@@ -14,8 +14,34 @@ public:
         : data(data1), next(nullptr) {}
 };
 
-Node *sortAllBetter(Node *head){
-    
+Node *sortAllBetter(Node *head)
+{
+    Node *zeroHead = new Node(-1);
+    Node *oneHead = new Node(-1);
+    Node *twoHead = new Node(-1);
+    Node *zero = zeroHead;
+    Node *one = oneHead;
+    Node *two = twoHead;
+
+    Node *temp = head;
+    while (temp)
+    {
+        if (temp->data == 0)
+        {
+            zero->next = temp;
+            zero = zero->next;
+        }
+        else if (temp->data == 1)
+        {
+            one->next = temp;
+            one = one->next;
+        }
+        else
+        {
+            two->next = temp;
+            two = two->next;
+        }
+    }
 }
 
 int main()
