acec5c26ccba187fd3ba83f0d5212842ed96a9de
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-30 19:02:45 +0530

    add: main function

--- 5. LinkedList/9-rmNthNodeFromEnd.cpp
+++ 5. LinkedList/9-rmNthNodeFromEnd.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 
-class Node{
+class Node
+{
 public:
     int data;
     Node *next;
 
 public:
     Node(int data1, Node *next1)
-        :data(data1), next(next1){}
+        : data(data1), next(next1) {}
 
     Node(int data1)
-        :data(data1), next(nullptr){}
+        : data(data1), next(nullptr) {}
 };
 
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+        Node *temp = new Node(x);
+
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+
+        else
+        {
+            tail->next = temp;
+            tail = temp->next;
+        }
+    }
+}
