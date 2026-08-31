6e5561d51066f00aba257ebc64bbe9ddd952cfc8
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-08-31 16:20:53 +0530

    beginning lecture 10 reverse LL

---
+++ 5. LinkedList/10-reverseLinkedList.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+        : data(data1), next(next1){}
+    
+    Node(int data1)
+        : data(data1), next(nullptr){}
+};
+
