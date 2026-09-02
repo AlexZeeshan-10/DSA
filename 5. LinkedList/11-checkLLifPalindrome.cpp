3dc4eb66d25e415ac93e389434b3fba0cf9aa7cd
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-02 19:44:57 +0530

    edit: main function

--- 5. LinkedList/11-checkLLifPalindrome.cpp
+++ 5. LinkedList/11-checkLLifPalindrome.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 
-class Node{
+class Node
+{
 public:
     int data;
     Node *next;
 
 public:
     Node(int data, Node *next)
-        : data(data), next(next){}
+        : data(data), next(next) {}
+
+    Node(int data)
+        : data(data), next(nullptr) {}
 };
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+        Node *temp = new Node(x);
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+
+        else
+        {
+            tail->next = temp;
+            tail = tail->next;
+        }
+    }
+}
