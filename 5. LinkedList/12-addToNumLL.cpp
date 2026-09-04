ab0107dddc9766936cd53cd01d4ac35dc6292e99
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-04 17:16:17 +0530

    add: function added

--- 5. LinkedList/12-addToNumLL.cpp
+++ 5. LinkedList/12-addToNumLL.cpp
@@ -26,7 +26,33 @@ Node *reverseNode(Node *curr){
     return prev;
 }
 
+Node *addOneBrute(Node *head){
+    head = reverseNode(head);
+    Node *temp = head;
+    int carry = 1;
 
+    while (temp)
+    {
+        temp->data += carry;
+        if(temp->data < 10){
+            carry = 0;
+            break;
+        }
+        else{
+            temp->data = 0;
+            carry = 1;
+        }
+        temp = temp->next;
+    }
+    if(carry == 1){
+        Node *newNode = new Node(1);
+        head = reverseNode(head);
+        newNode->next = head;
+        return newNode;
+    }
+    head = reverseNode(head);
+    return head;
+}
 
 int main()
 {
@@ -53,6 +79,8 @@ int main()
             tail = tail->next;
         }
     }
+
+    head = addOneBrute(head);
 
     Node *temp = head;
     while (temp)
