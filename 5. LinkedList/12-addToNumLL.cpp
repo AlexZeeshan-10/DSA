66ad87ce050a2fb13e2bf54c2fd3eb20faa12e6f
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-04 15:38:46 +0530

    added main function

--- 5. LinkedList/12-addToNumLL.cpp
+++ 5. LinkedList/12-addToNumLL.cpp
@@ -13,3 +13,38 @@ public:
     Node(int data)
         : data(data), next(nullptr) {}
 };
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+        Node *temp = new Node(x);
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+
+        else
+        {
+            tail->next = temp;
+            tail = tail->next;
+        }
+    }
+
+    Node *temp = head;
+    while (temp)
+    {
+        std::cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    std::cout << "nullptr";
+}
