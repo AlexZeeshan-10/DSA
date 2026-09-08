f85469b8b3959b10f07a697b220c20cd5e6b1966
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-08 23:53:09 +0530

    add: added function

--- 5. LinkedList/13-findIntersectionLL.cpp
+++ 5. LinkedList/13-findIntersectionLL.cpp
@@ -17,7 +17,25 @@ public:
 
 Node *intersectionNodeBrute(Node *head1, Node *head2)
 {
-    
+    std::map<Node *, int> mpp;
+    Node *temp = head1;
+
+    while (temp)
+    {
+        mpp[temp] = 1;
+        temp = temp->next;
+    }
+
+    temp = head2;
+    while (temp)
+    {
+        if (mpp.find(temp) != mpp.end())
+            return temp;
+
+        temp = temp->next;
+    }
+
+    return nullptr;
 }
 
 int main()
