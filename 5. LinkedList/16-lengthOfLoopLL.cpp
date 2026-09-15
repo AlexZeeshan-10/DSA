af7c667de8e913f095a59b041b0763cc6a57392b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-15 19:52:27 +0530

    formatted

---
+++ 5. LinkedList/16-lengthOfLoopLL.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+        : data(data1), next(next1) {}
+
+    Node(int data1)
+        : data(data1), next(nullptr) {}
+};
+
+int 
+
+int main(){
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+
+        Node *temp = new Node(x);
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+
+        else
+        {
+            tail->next = temp;
+            tail = tail->next;
+        }
+    }
+}
