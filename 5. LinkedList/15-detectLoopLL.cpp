be37642f589d57239c43f24e4dbc733fec11da1b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-15 19:25:02 +0530

    add: brute function added

--- 5. LinkedList/15-detectLoopLL.cpp
+++ 5. LinkedList/15-detectLoopLL.cpp
@@ -24,6 +24,16 @@ bool hasCycle(Node *head)
 
     Node *temp = head;
     std::unordered_map<Node *, int> mpp;
+    while (temp)
+    {
+        if (mpp.find(temp) == mpp.end())
+            return true;
+        else
+            mpp[temp] = 1;
+
+        temp = temp->next;
+    }
+    return false;
 }
 
 int main()
