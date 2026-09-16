ff556d9c68fc63404441fa83fc0d6fc7910546fb
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-16 21:07:27 +0530

    defined the function to delete the key in Doubly LL

--- 5. LinkedList/19-deleteOccurenceOfKeyDLL.cpp
+++ 5. LinkedList/19-deleteOccurenceOfKeyDLL.cpp
@@ -15,8 +15,37 @@ public:
         : prev(nullptr), data(data1), next(nullptr) {}
 };
 
-Node *deleteOccurence(Node *head, int k){
-    
+Node *deleteOccurence(Node *head, int k)
+{
+    if (head == nullptr)
+        return nullptr;
+
+    Node *temp = head;
+    while (temp)
+    {
+        if (temp->data == k)
+        {
+            if (temp == head)
+            {
+                head = head->next;
+            }
+            Node *nextNode = temp->next;
+            Node *prevNode = temp->prev;
+
+            if (nextNode)
+                nextNode->prev = prevNode;
+            if (prevNode)
+                prevNode->next = nextNode;
+
+            delete temp;
+            temp = nextNode;
+        }
+        else
+        {
+            temp = temp->next;
+        }
+    }
+    return head;
 }
 
 int main()
@@ -54,7 +83,7 @@ int main()
         std::cout << temp->data << " <-> ";
         temp = temp->next;
     }
-    std::cout << "nullptr";
+    std::cout << "nullptr" << std::endl;
 
     int k;
     std::cin >> k;
@@ -62,7 +91,7 @@ int main()
     head = deleteOccurence(head, k);
 
     temp = head;
-    std::cout << "nullptr <-> ";
+    std::cout << std::endl << "nullptr <-> ";
     while (temp)
     {
         std::cout << temp->data << " <-> ";
