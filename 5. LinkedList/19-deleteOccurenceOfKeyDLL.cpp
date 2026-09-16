c2c89eba8fd293e4e53ea48912fe441f8ea1fa53
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-16 19:34:15 +0530

    adding main function to lecture 19 delete occurence in Doubly LL

---
+++ 5. LinkedList/19-deleteOccurenceOfKeyDLL.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+public:
+    Node(Node *prev1, int data1, Node *next1)
+        : prev(prev1), data(data1), next(next1) {}
+
+    Node(Node *prev1, int data1)
+        : prev(prev1), data(data1), next(nullptr) {}
+
+    Node(int data1, Node *next1)
+        : prev(nullptr), data(data1), next(next1) {}
+
+    Node(int data1)
+        : prev(nullptr), data(data1), next(nullptr) {}
+};
+
+int main(){
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    
+}
