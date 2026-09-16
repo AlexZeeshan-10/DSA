9a0dd79edf75e3a8648529d2291fa16c73ce1957
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-16 20:39:35 +0530

    main function defined completely

--- 5. LinkedList/19-deleteOccurenceOfKeyDLL.cpp
+++ 5. LinkedList/19-deleteOccurenceOfKeyDLL.cpp
@@ -11,22 +11,60 @@ public:
     Node(Node *prev1, int data1, Node *next1)
         : prev(prev1), data(data1), next(next1) {}
 
-    Node(Node *prev1, int data1)
-        : prev(prev1), data(data1), next(nullptr) {}
-
-    Node(int data1, Node *next1)
-        : prev(nullptr), data(data1), next(next1) {}
-
     Node(int data1)
         : prev(nullptr), data(data1), next(nullptr) {}
 };
 
-int main(){
+int main()
+{
     int n;
     std::cin >> n;
 
     Node *head = nullptr;
     Node *tail = nullptr;
 
-    
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+
+        Node *temp = new Node(x);
+
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else
+        {
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+    }
+
+    Node *temp = head;
+    std::cout << "nullptr <-> ";
+    while (temp)
+    {
+        std::cout << temp->data << " <-> ";
+        temp = temp->next;
+    }
+    std::cout << "nullptr";
+
+    int k;
+    std::cin >> k;
+
+    head = deleteOccurence(head);
+
+    temp = head;
+    std::cout << "nullptr <-> ";
+    while (temp)
+    {
+        std::cout << temp->data << " <-> ";
+        temp = temp->next;
+    }
+    std::cout << "nullptr";
+
+    return 0;
 }
