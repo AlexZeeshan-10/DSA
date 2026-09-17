ba04bdc44e1fdbcd08edfe67a15be576f8d41f2b
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-17 19:56:03 +0530

    added the brute function to find the sum

--- 5. LinkedList/20-findPairsWithGivenSumDLL.cpp
+++ 5. LinkedList/20-findPairsWithGivenSumDLL.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class Node
 {
@@ -14,6 +15,25 @@ public:
     Node(int data1)
         : prev(nullptr), data(data1), next(nullptr) {}
 };
+
+std::vector<std::vector<int>> findPair(Node *head, int sum)
+{
+    std::vector<std::vector<int>> ans;
+    Node *temp1 = head;
+    while (temp1)
+    {
+        Node *temp2 = temp1->next;
+        while (temp2)
+        {
+            if (temp1->data + temp2->data == sum)
+                ans.push_back({temp1->data, temp2->data});
+
+            temp2 = temp2->next;
+        }
+        temp1 = temp1->next;
+    }
+    return ans;
+}
 
 int main()
 {
@@ -47,5 +67,21 @@ int main()
         std::cout << temp->data << " <-> ";
         temp = temp->next;
     }
-    std::cout << "nullptr";
+    std::cout << "nullptr" << std::endl;
+
+    int sum;
+    std::cin >> sum;
+
+    std::vector<std::vector<int>> ans = findPair(head, sum);
+
+    for (size_t i = 0; i < ans.size(); ++i)
+    {
+        for (size_t j = 0; j < ans[i].size(); ++j)
+        {
+            std::cout << ans[i][j] << " ";
+        }
+
+        std::cout << '\n';
+    }
+    return 0;
 }
