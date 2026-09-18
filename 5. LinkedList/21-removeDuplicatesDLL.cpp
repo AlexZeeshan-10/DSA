4fc93198459e0322f1d0cf3a76b2e44b6e9ce247
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-18 14:27:36 +0530

    main function edited

--- 5. LinkedList/21-removeDuplicatesDLL.cpp
+++ 5. LinkedList/21-removeDuplicatesDLL.cpp
@@ -3,18 +3,51 @@
 class Node
 {
 public:
-    int data;
+    int val;
     Node *prev;
     Node *next;
 
 public:
-    Node(Node *prev1, int data1, Node *next1)
-        : prev(prev1), data(data1), next(next1) {}
+    Node(Node *prev1, int val1, Node *next1)
+        : prev(prev1), val(val1), next(next1) {}
 
-    Node(int data1)
-        : prev(nullptr), data(data1), next(nullptr) {}
+    Node(int val1)
+        : prev(nullptr), val(val1), next(nullptr) {}
 };
 
-int main(){
-    
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+        Node *temp = new Node(x);
+
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else
+        {
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+    }
+
+    Node *temp = head;
+    std::cout << "nullptr <-> ";
+    while (temp)
+    {
+        std::cout << temp->val << " <-> ";
+        temp = temp->next;
+    }
+    std::cout << "nullptr";
 }
