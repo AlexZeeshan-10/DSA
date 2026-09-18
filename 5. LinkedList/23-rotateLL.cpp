3822f18ef31f7ad26a9a46b46c90b20fa4c08b8a
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-18 19:40:13 +0530

    main function added for lecture 23 rotate a LL

---
+++ 5. LinkedList/23-rotateLL.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+public:
+    Node(Node *prev1, int val1, Node *next1)
+        : prev(prev1), val(val1), next(next1) {}
+
+    Node(int val1)
+        : prev(nullptr), val(val1), next(nullptr) {}
+};
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    while (n--)
+    {
+        int x;
+        std::cin >> x;
+        Node *temp = new Node(x);
+        if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else
+        {
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+    }
+
+    Node *temp = head;
+    std::cout << "nullptr <-> ";
+    while (temp)
+    {
+        std::cout << temp->val << " <-> ";
+        temp = temp->next;
+    }
+    std::cout << "nullptr" << std::endl;
+
+    int k;
+    std::cin >> k;
+}
